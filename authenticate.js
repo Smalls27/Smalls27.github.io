fd2f57d18fe30ca69740618ad241725b9379104d
Kenneth Smalls <kennethsmalls244@gmail.com>
2021-07-24 12:55:23 -0400

    more changes

--- authenticate.js
+++ authenticate.js
@@ -1,20 +1,19 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
 const User = require('./schemas/userSchema');
 
 
 passport.use(new LocalStrategy(function (username, password, done) {
 	User.findOne({ username: username }, function (err, user) {
+		const loginPassword = password;
 		if (err) return done(err);
 		if (!user) return done(null, false, { message: 'Incorrect username.' });
 
-		bcrypt.compare(password, user.password, function (err, res) {
-			if (err) return done(err);
-			if (res === false) return done(null, false, { message: 'Incorrect password.' });
-			
+		if (loginPassword === user.password) {
 			return done(null, user);
-		});
+		} else {
+		 	return done(null, false, { message: 'Incorrect password.' });
+		}
 	});
 }));
 
